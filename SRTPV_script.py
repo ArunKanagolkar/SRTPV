edf17a710786a2e55163a321271aa2ee2ef87c38
Arun Kanagolkar <138815437+ArunKanagolkar@users.noreply.github.com>
2025-01-23 18:29:43 +0530

    Update SRTPV_script.py

--- SRTPV_script.py
+++ SRTPV_script.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 df = pd.read_csv('SRTPV_Fees.csv')
-st.title("Estimating SRTPV Capacity")
+st.title("Quik Estimator for SRTPV")
 st.subheader('Solar rooftop PV module')
 st.image("srtpv.jpeg", caption="Sunrise for the clean energy")
 st.write('Allowed area for SRTPV installation')
 all = st.selectbox('Allowed area',['Residential','Non-Residential','Apartments','Carport'])
 if all=='Residential':
-  st.write('*Avail for PM Surya Ghar Scheme subsidy for SRTPV installation*:sunglasses:')
+  st.write('*Qualified to receive SRTPV installation subsidies under the PM Surya Ghar Scheme*:sunglasses:')
 else:
   st.write('*No subsidy is available*')
 st.write('Application & Facilitation Fees')
@@ -31,8 +31,8 @@ if st.button("How much solar energy is produced each year?"):
   st.write(eg,"Units")
 if st.button('Maximun Capacity'):
   et = area/8
-  st.write('Your roofspace available for SRTPV installation upto in kWp:')
-  st.write(et)                    
+  st.write('Your roof area can be used to install SRTPVs up to',et,'kWp')
+                     
 if st.button('Check power evacuation'):
  if sl > 150:
    st.write('Proposed SRTPV capacity is morethan 150KW, the consumer shall convert existing distribution system into 11KV')
